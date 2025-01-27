dbdb1117449c750f707cbcba437233849cdfe059
Vladyslav <vladoromus@gmail.com>
2025-01-27 01:00:47 +0100

    solution

--- app/main.py
+++ app/main.py
@@ -1,12 +1,18 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+    def __init__(self, comfort_class: int,
+                 clean_mark: int,
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: int, clean_power: int, average_rating: int, count_of_ratings: int) -> None:
+    def __init__(self,
+                 distance_from_city_center: int,
+                 clean_power: int,
+                 average_rating: int,
+                 count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -15,14 +21,20 @@ class CarWashStation:
     def calculate_washing_price(self, car: object) -> int:
         if car.clean_mark >= self.clean_power:
             return 0.0
-        return round(car.comfort_class * (self.clean_power - car.clean_mark) * (self.average_rating / self.distance_from_city_center), 1)
+        return round(car.comfort_class
+                     * (self.clean_power - car.clean_mark)
+                     * (self.average_rating
+                        / self.distance_from_city_center), 1)
 
     def wash_single_car(self, car: object) -> None:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
 
     def rate_service(self, rate: int) -> None:
-        self.average_rating = round((self.average_rating * self.count_of_ratings + rate) / (self.count_of_ratings + 1), 1)
+        self.average_rating = round((self.average_rating
+                                     * self.count_of_ratings
+                                     + rate)
+                                    / (self.count_of_ratings + 1), 1)
         self.count_of_ratings += 1
 
     def serve_cars(self, cars: list) -> float:
